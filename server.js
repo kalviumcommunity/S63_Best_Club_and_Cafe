dc7d56a7d6c346a5787022ac4f0fcd2aa1764087
tejraj.singh.s63@kalvium.community <you@example.com>
2025-01-29 12:24:24 +0530

    bruno update

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    console.log("hello world printed");
-    res.send('Hello, World!'); // Respond with "Hello, World!" when a GET request is made to the root URL ('/') of the server 
+app.get('/Ping', (req, res) => {
+    // console.log("pong");
+    res.send('Pong'); // Respond with "Hello, World!" when a GET request is made to the root URL ('/') of the server 
 });
     
-    app.listen(5000, () => { // Start the server on port 3000 when the application is ready 
+    app.listen(3000, () => { // Start the server on port 3000 when the application is ready 
     console.log('Server is running on port 3000');
 });
